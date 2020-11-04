58b2931a1900357fb4b536d5bea422874365aae4
leesunyong <55078646+leesunyong@users.noreply.github.com>
2020-11-04 09:52:09 +0900

    Tags and requirements.txt

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from data_process import process_data_file
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Bi Sentence to Vector')
+    parser.add_argument('module', type=str)
     parser.add_argument('input', type=str,
                         help='data file for embedding words')
 
     args = parser.parse_args()
 
-    process_data_file(args.input, 'ko', 'en')
+    if args.module == 'process':
+        process_data_file(args.input, 'ko', 'en')
