4a022a86f4aa8451378cb0de0a090f651dae2ba3
leesunyong <55078646+leesunyong@users.noreply.github.com>
2020-11-12 11:39:18 +0900

    setup.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+import setup
 import argparse
-from data_process import process_data_file
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Bi Sentence to Vector')
@@ -8,12 +9,18 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.module == 'parse':
+    if args.module == 'download':
+        # Download all requirements
+        setup.download()
+
+    elif args.module == 'parse':
+        from data_process import process_data_file
+
         # params : input file names
         # input file contents format : [Korean Sentence] \t [English Sentence]
         #   Two sentences in different languages must be split by '\t'
         open('ko-en.txt', 'w').close()
         for file_name in args.params:
             process_data_file(file_name, 'ko', 'en')
-    # elif args.module == 'fasttext':
-    #     fasttext('ko', 'en')
+#     # elif args.module == 'fasttext':
+#     #     fasttext('ko', 'en')
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+
+
+def download():
+    _package_download()
+
+    if not os.path.isdir('./khaiii'):
+        _khaiii_download()
+    else:
+        print("khaiii is already installed.")
+
+
+def _package_download():
+    # install requirements
+    print("Download python packages.")
+    os.system("pip3 install -r requirements.txt")
+
+
+def _khaiii_download():
+    # download khaiii from Github
+    print("Download Khaiii")
+    os.system("git clone https://github.com/kakao/khaiii.git")
+
+    # build python
+    os.system("mkdir khaiii/build")
+    os.system("cd khaiii/build && cmake ..")
+
+    os.system("cd khaiii/build && make all && make resource && ctest")
+
+    # Python binding
+    os.system("cd khaiii/build && make package_python")
+    os.system("cd khaiii/build/package_python && pip3 install .")
